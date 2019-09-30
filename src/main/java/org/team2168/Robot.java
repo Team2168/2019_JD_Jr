e1b089cce82ce17f25ebef175222e0305d661a3c
WilliamZieg03 <williamzieg03@gmail.com>
2019-09-30 19:13:20 -0400

    Initiating the Pivot Subsystem in Robot

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -39,6 +39,9 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    pivot = Pivot.getinstance();
+    
   }
 
   /**
